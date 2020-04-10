39891298962f51de692cd7c245319148ca90c6e2
Rakesh Kumar T <rakesht2499@gmail.com>
2020-04-11 02:03:46 +0530

    Adding bootstrap

--- loginpage/login/views.py
+++ loginpage/login/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
-from django.http import HttpResponse
 from login.models import log
+
 
 # Create your views here.
 class loginpage(TemplateView):
     template_name = 'loginpage.html'
+
 
 def data(request):
     user = str(request.POST['UserName'])
@@ -13,9 +14,11 @@ def data(request):
     try:
         t = log.objects.get(studentcode = user)
         if(str(t.dob) == password):
-            return render(request,'loggedinpage.html',{'code': t.studentcode , 'name': t.studentname , 'admin': t.adminno , 'class' : t.classname ,
-            'section': t.section , 'result' : t.result})
+            return render(request,'loggedinpage.html',{'code': t.studentcode , 'name': t.studentname ,
+                                                       'admin': t.adminno , 'class' : t.classname ,
+                                                       'section': t.section , 'result' : t.result,
+                                                       'pass': t.result == "PASS"})
         else:
-            return HttpResponse("loginfailed ")
+            return render(request, 'loginpage.html', {'login_fail': True})
     except:
-        return HttpResponse("loginfailed")
+        return render(request, 'loginpage.html', {'login_fail': True})
